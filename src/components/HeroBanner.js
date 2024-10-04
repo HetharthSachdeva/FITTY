e97e384b572ebb8bc1c734c61a0a176f1558a6b2
vedhem999 <161755713+vedantDube@users.noreply.github.com>
2024-10-04 12:42:21 +0530

    push to hetharth

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { Box, Stack, Typography } from '@mui/material';
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-import HeroBannerImage from '../assets/images/banner.png';
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => (
-  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#3333FF" fontWeight="1000" fontSize="36px" fontFamily="Roboto">FITTY</Typography>
-    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
-      Commit, Train<br />
+  <Box
+    sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+    position="relative"
+    p="20px"
+  >
+    <Typography
+      color="#3333FF"
+      fontWeight="1000"
+      fontSize="36px"
+      fontFamily="Roboto"
+    >
+      FITTY
+    </Typography>
+    <Typography
+      fontWeight={700}
+      sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      mb="23px"
+      mt="30px"
+    >
+      Commit, Train
+      <br />
       And Succeed
     </Typography>
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
@@ -17,34 +34,42 @@ const HeroBanner = () => (
       <a
         href="#exercises"
         style={{
-          marginTop: '45px',
-          textDecoration: 'none',
-          width: '200px',
-          textAlign: 'center',
-          background: '#3333FF',
-          padding: '14px',
-          fontSize: '22px',
-          textTransform: 'none',
-          color: 'white',
-          borderRadius: '4px',
-          transition: 'background 0.3s, color 0.3s smooth',
+          marginTop: "45px",
+          textDecoration: "none",
+          width: "200px",
+          textAlign: "center",
+          background: "#3333FF",
+          padding: "14px",
+          fontSize: "22px",
+          textTransform: "none",
+          color: "white",
+          borderRadius: "4px",
+          transition: "background 0.3s, color 0.3s smooth",
         }}
         onMouseEnter={(e) => {
-          e.target.style.background = '#FFFAFB';
-          e.target.style.color = '#3333FF';
-          e.target.style.border = '3px solid #3333FF';
-          e.target.style.transform = 'translateY(-3px)';
+          e.target.style.background = "#FFFAFB";
+          e.target.style.color = "#3333FF";
+          e.target.style.border = "3px solid #3333FF";
+          e.target.style.transform = "translateY(-3px)";
         }}
         onMouseLeave={(e) => {
-          e.target.style.background = '#3333FF';
-          e.target.style.color = 'white';
-          e.target.style.transform = 'translateY(3px)';
+          e.target.style.background = "#3333FF";
+          e.target.style.color = "white";
+          e.target.style.transform = "translateY(3px)";
         }}
       >
         Explore Exercises
       </a>
     </Stack>
-    <Typography fontWeight={600} color="#3333FF" sx={{ opacity: '0.05', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+    <Typography
+      fontWeight={600}
+      color="#3333FF"
+      sx={{
+        opacity: "0.05",
+        display: { lg: "block", xs: "none" },
+        fontSize: "200px",
+      }}
+    >
       GRIND
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
