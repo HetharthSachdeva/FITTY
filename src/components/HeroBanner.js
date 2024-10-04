2d8282b5103e87f4215af06cbe664a63277c974a
Hetharth Sachdeva <hetharthsach@gmail.com>
2024-10-04 13:06:03 +0530

    Fixed Vedant Dubey's Error

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-import HeroBannerImage from "../assets/images/banner.png";
+import HeroBannerImage from '../assets/images/banner.png';
 
 const HeroBanner = () => (
   <Box
-    sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+    sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }}
     position="relative"
     p="20px"
   >
@@ -19,7 +19,7 @@ const HeroBanner = () => (
     </Typography>
     <Typography
       fontWeight={700}
-      sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      sx={{ fontSize: { lg: '44px', xs: '40px' } }}
       mb="23px"
       mt="30px"
     >
@@ -34,28 +34,28 @@ const HeroBanner = () => (
       <a
         href="#exercises"
         style={{
-          marginTop: "45px",
-          textDecoration: "none",
-          width: "200px",
-          textAlign: "center",
-          background: "#3333FF",
-          padding: "14px",
-          fontSize: "22px",
-          textTransform: "none",
-          color: "white",
-          borderRadius: "4px",
-          transition: "background 0.3s, color 0.3s smooth",
+          marginTop: '45px',
+          textDecoration: 'none',
+          width: '200px',
+          textAlign: 'center',
+          background: '#3333FF',
+          padding: '14px',
+          fontSize: '22px',
+          textTransform: 'none',
+          color: 'white',
+          borderRadius: '4px',
+          transition: 'background 0.3s, color 0.3s smooth',
         }}
         onMouseEnter={(e) => {
-          e.target.style.background = "#FFFAFB";
-          e.target.style.color = "#3333FF";
-          e.target.style.border = "3px solid #3333FF";
-          e.target.style.transform = "translateY(-3px)";
+          e.target.style.background = '#FFFAFB';
+          e.target.style.color = '#3333FF';
+          e.target.style.border = '3px solid #3333FF';
+          e.target.style.transform = 'translateY(-3px)';
         }}
         onMouseLeave={(e) => {
-          e.target.style.background = "#3333FF";
-          e.target.style.color = "white";
-          e.target.style.transform = "translateY(3px)";
+          e.target.style.background = '#3333FF';
+          e.target.style.color = 'white';
+          e.target.style.transform = 'translateY(3px)';
         }}
       >
         Explore Exercises
@@ -65,9 +65,9 @@ const HeroBanner = () => (
       fontWeight={600}
       color="#3333FF"
       sx={{
-        opacity: "0.05",
-        display: { lg: "block", xs: "none" },
-        fontSize: "200px",
+        opacity: '0.05',
+        display: { lg: 'block', xs: 'none' },
+        fontSize: '200px',
       }}
     >
       GRIND
